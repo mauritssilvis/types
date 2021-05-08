0d196a50ae547241ee4d439d0fa11f133decb13a
Maurits Silvis <mauritssilvis@gmail.com>
2021-05-08 11:03:20 +0200

    Rename the string test variables

--- java/src/test/java/nl/mauritssilvis/types/string/ArrayStringTests.java
+++ java/src/test/java/nl/mauritssilvis/types/string/ArrayStringTests.java
@@ -13,24 +13,24 @@ class ArrayStringTests {
     @Test
     void storeAnIndependentCopyOfTheCharacters() {
         char[] chars = {'s', 'u', 'n'};
-        String word = new ArrayString(chars);
+        String str = new ArrayString(chars);
 
         int index = 1;
         chars[index] = 'o';
 
-        Assertions.assertNotEquals(chars[index], word.charAt(index));
+        Assertions.assertNotEquals(chars[index], str.charAt(index));
     }
 
     @Test
     void storeAllCharacters() {
         char[] chars = {'s', 'k', 'y'};
-        String word = new ArrayString(chars);
+        String str = new ArrayString(chars);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(chars.length, word.length()),
-                () -> Assertions.assertEquals(chars[0], word.charAt(0)),
-                () -> Assertions.assertEquals(chars[1], word.charAt(1)),
-                () -> Assertions.assertEquals(chars[2], word.charAt(2))
+                () -> Assertions.assertEquals(chars.length, str.length()),
+                () -> Assertions.assertEquals(chars[0], str.charAt(0)),
+                () -> Assertions.assertEquals(chars[1], str.charAt(1)),
+                () -> Assertions.assertEquals(chars[2], str.charAt(2))
         );
     }
 
@@ -42,64 +42,64 @@ class ArrayStringTests {
 
     @Test
     void storeTheFullJavaString() {
-        java.lang.String str = "moon";
-        String word = new ArrayString(str);
+        java.lang.String javaStr = "moon";
+        String str = new ArrayString(javaStr);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(str.length(), word.length()),
-                () -> Assertions.assertEquals(str.charAt(0), word.charAt(0)),
-                () -> Assertions.assertEquals(str.charAt(1), word.charAt(1)),
-                () -> Assertions.assertEquals(str.charAt(2), word.charAt(2)),
-                () -> Assertions.assertEquals(str.charAt(3), word.charAt(3))
+                () -> Assertions.assertEquals(javaStr.length(), str.length()),
+                () -> Assertions.assertEquals(javaStr.charAt(0), str.charAt(0)),
+                () -> Assertions.assertEquals(javaStr.charAt(1), str.charAt(1)),
+                () -> Assertions.assertEquals(javaStr.charAt(2), str.charAt(2)),
+                () -> Assertions.assertEquals(javaStr.charAt(3), str.charAt(3))
         );
     }
 
     @Test
     void doNotGetACharacterWithANegativeIndex() {
         char[] chars = {'l', 'a', 'k', 'e'};
-        String word = new ArrayString(chars);
+        String str = new ArrayString(chars);
 
         int index = -1;
 
-        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> word.charAt(index));
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> str.charAt(index));
     }
 
     @Test
     void doNotGetACharacterBeyondTheStringLength() {
         char[] chars = {'m', 'o', 'u', 'n', 't', 'a', 'i', 'm'};
-        String word = new ArrayString(chars);
+        String str = new ArrayString(chars);
 
         int index = chars.length;
 
-        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> word.charAt(index));
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> str.charAt(index));
     }
 
     @Test
     void getEachCharacter() {
         char[] chars = {'l', 'a', 'n', 'd'};
-        String word = new ArrayString(chars);
+        String str = new ArrayString(chars);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(chars[0], word.charAt(0)),
-                () -> Assertions.assertEquals(chars[1], word.charAt(1)),
-                () -> Assertions.assertEquals(chars[2], word.charAt(2)),
-                () -> Assertions.assertEquals(chars[3], word.charAt(3))
+                () -> Assertions.assertEquals(chars[0], str.charAt(0)),
+                () -> Assertions.assertEquals(chars[1], str.charAt(1)),
+                () -> Assertions.assertEquals(chars[2], str.charAt(2)),
+                () -> Assertions.assertEquals(chars[3], str.charAt(3))
         );
     }
 
     @Test
     void getTheLengthOfAZeroCharacterString() {
         char[] chars = {};
-        String word = new ArrayString(chars);
+        String str = new ArrayString(chars);
 
-        Assertions.assertEquals(chars.length, word.length());
+        Assertions.assertEquals(chars.length, str.length());
     }
 
     @Test
     void getTheLengthOfAFourCharacterString() {
         char[] chars = {'h', 'i', 'l', 'l'};
-        String word = new ArrayString(chars);
+        String str = new ArrayString(chars);
 
-        Assertions.assertEquals(chars.length, word.length());
+        Assertions.assertEquals(chars.length, str.length());
     }
 }
