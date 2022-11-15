4b16cdbeaa9e906531afb3a596d12013e9502808
Maurits Silvis <mauritssilvis@gmail.com>
2022-11-15 19:32:58 +0100

    Use string fields where possible

--- java/src/main/java/nl/mauritssilvis/types/java/string/ArrayString.java
+++ java/src/main/java/nl/mauritssilvis/types/java/string/ArrayString.java
@@ -82,6 +82,6 @@ public class ArrayString implements ImmutableString {
 
     @Override
     public String toString() {
-        return new String(toCharArray());
+        return new String(chars.clone());
     }
 }
