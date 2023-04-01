ad836fa77e4b1c1b604ab5cd1854d805c91f1f51
Maurits Silvis <mauritssilvis@gmail.com>
2023-04-01 16:38:45 +0200

    Configure a toolchain resolver

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -16,7 +16,15 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
+        vendor.set(JvmVendorSpec.ADOPTIUM)
+    }
 }
 
 tasks {
