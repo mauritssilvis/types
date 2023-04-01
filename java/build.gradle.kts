37df914e26dedbd07c9c5a140a8a4a59bb46ad17
Maurits Silvis <mauritssilvis@gmail.com>
2023-04-01 16:53:07 +0200

    Use the Gradle refreshVersions plugin

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -15,9 +15,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation(libs.junit.jupiter.api)
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
 java {
